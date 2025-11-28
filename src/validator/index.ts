6d4fbbfc95800661b4bd765303131771f3789738
boris-fouomene <borisfouomen14@gmail.com>
2025-11-28 15:00:31 +0100

    refactor: remove unused classes and imports from validator module

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,22 +1,7 @@
 import 'reflect-metadata';
-import { ArrayOf, ensureRulesRegistered, OneOf } from './rules';
-import { Validator } from './validator';
+import { ensureRulesRegistered } from './rules';
 // Ensure rules are loaded
 ensureRulesRegistered();
 export * from './rules';
 export * from './types';
 export * from './validator';
-
-class Address {
-  street: string = '';
-}
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-class User {
-  @ArrayOf(['Array', 'ArrayAllNumbers'])
-  @OneOf([
-    'NonNullString',
-    { MinLength: [10] },
-    Validator.validateNested<typeof Address>(Address),
-  ])
-  address: Address = new Address();
-}
